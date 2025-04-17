5e70220206955b5b10eb785064bad8c4a4f44281
User <id55i@yandex.ru>
2025-04-18 02:43:41 +0300

    время и дата без лишней фигни

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from telegram.ext import Application, MessageHandler, filters
 from telegram.ext import CommandHandler
 from datetime import datetime
 
-
+# Запускаем логгирование
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
@@ -11,43 +11,20 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-async def echo(update, context):
-    await update.message.reply_text(f'Я получил сообщение <{update.message.text}>')
-
-
-async def start(update, context):
-    """Отправляет сообщение когда получена команда /start"""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Привет {user.mention_html()}! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!",
-    )
-
-
-async def help_command(update, context):
-    """Отправляет сообщение когда получена команда /help"""
-    await update.message.reply_text("Я пока не умею помогать... Я только ваше эхо.")
-
-
 async def time_command(update, context):
-    """Отправляет сообщение когда получена команда /time"""
     now = datetime.now().strftime("%H:%M:%S")
     await update.message.reply_text(now)
 
 
 async def data_command(update, context):
-    """Отправляет сообщение когда получена команда /data"""
-    now = datetime.now().strftime("%Y:%m:%d").replace(':', '-')
+    now = datetime.now().strftime("%Y:%m:%d").replace(':', '.')
     await update.message.reply_text(now)
 
 
 def main():
     application = Application.builder().token('7925593712:AAEh8X0Z3twKdGJJJ4bz1mDxMQ1nAyEKCZM').build()
-    text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
     application.add_handler(CommandHandler("time", time_command))
     application.add_handler(CommandHandler("data", data_command))
-    application.add_handler(text_handler)
     application.run_polling()
 
 
