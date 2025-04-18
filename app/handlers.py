3a2a621bd51ce594e41d7fdb848d3d37cb9e2f57
ullvea <sofimastikova@gmail.com>
2025-04-18 12:16:07 +0300

    handlers in new file

---
+++ app/handlers.py
@@ -0,0 +1,60 @@
+import asyncio
+from aiogram import F, Router
+from aiogram.enums import parse_mode
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+
+router = Router()
+
+
+@router.message(CommandStart())  # декоратор для обработчика команды start
+async def process_start_command(message: Message):
+    await message.reply(
+        f"Привет, @{message.from_user.username}! 👋 \n Я бот-парсер, готов помочь тебе собрать нужную информацию."
+        " Скажи, что именно ты хочешь найти?")  # отправляет ответ на сообщение
+
+
+@router.message(Command('help'))  # декоратор для обработчика команды help
+async def process_help_command(message: Message):
+    await message.reply("<b>Что может этот бот?</b>\n\n"
+                        "📌 помочь тебе сравнить цены товаров на различных маркетплейсах.\n"
+                        "📌 узнать как изменялась цена товар последнее время\n"
+                        "📌 узнать какие ещё есть похожие товары по этому запросу\n\n"
+                        
+                        "<b>Какие есть быстрые команды?</b>\n"
+                        "\\help - помощь по боту\n"
+                        "\\my_goods - товары, обавленные для отслеживания цен\n"
+                        "\n"
+
+                        "<b>Что же можешь сделать ты:</b>\n"
+                        "📌 можешь отправить мне название товара или же скинуть его фотку, "
+                        "в ответ я скину тебе информацию об этом товаре из разных источников.",
+                        parse_mode="HTML")
+
+
+@router.message(F.photo)  # функция для получения фотографий
+async def get_photo(message: Message):
+    await message.answer(message.photo[1].file_id)
+
+
+@router.message(Command('get_photo'))
+async def get_photo(message: Message):
+    await message.answer_photo(
+        photo="AgACAgIAAxkBAANjaAIHz78dRbUq3xqJlQ99XFKSliEAAobrMRsb6BFIK2F21syNTTEBAAMCAANtAAM2BA",
+        caption='так выглядит подпись')
+    # где фото можно указать ссылку из гугла
+
+
+@router.message()  # декоратор для обработчика прочих сообщений
+async def echo_message(message: Message, bot):
+    response_message = await message.answer(f'Запрос принят, @{message.from_user.username}!\n'
+                                            '💭Ещё чуть-чуть, готовлю ответ')
+    # отправляет сообщение об обработке запроса, а затем удаляет его пока 60 сек, в дальнейшим пока
+    # не сгенерируется запрос
+    await delete_message(response_message.chat.id, response_message.message_id, 5, bot)
+
+
+# Функция для удаления сообщения через заданное время
+async def delete_message(chat_id, message_id, delay, bot):
+    await asyncio.sleep(delay)
+    await bot.delete_message(chat_id, message_id)
