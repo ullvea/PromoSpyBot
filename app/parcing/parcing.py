1800e58efc73be508f4f27d02a24c23532ed5279
ullvea <sofimastikova@gmail.com>
2025-04-20 20:26:25 +0300

    parcing ymarket is done :)

--- app/parcing/parcing.py
+++ app/parcing/parcing.py
@@ -22,12 +22,14 @@ OZON_url = "https://ozon.ru"
 WB_url = "https://www.wildberries.ru/"
 YMarket_url = "https://market.yandex.ru/"
 
-TIME_SLEEP = 2
+TIME_SLEEP = 3
 TIME_WAIT = 5
 page_scroll_down_times = 10
 
+
 class ParcingError(Exception):
     pass
+
 
 def scroll(driver):
     for _ in range(page_scroll_down_times):
@@ -54,8 +56,80 @@ def get_info_Ymarket(product):
     input_window.send_keys(Keys.ENTER)
     time.sleep(TIME_SLEEP)
 
+    # <div data-cs-name="navigate"
+
+    item_links = driver.find_elements(By.CSS_SELECTOR, '[data-auto="galleryLink"]')
+    item_links = list(set([f'{item.get_attribute("href")}' for item in item_links]))
+    print(item_links)
+
+    for link in item_links:
+        info_of_product = find_info_item_ymarket(driver, link)
+        print(info_of_product)
+        return
+
     driver.close()
     driver.quit()
+
+
+def find_info_item_ymarket(driver, link):
+    driver.switch_to.new_window('tab')
+    driver.get(link)
+
+    soup = bs(str(driver.page_source), "lxml")
+    with open('one_pr_ym.html', 'w', encoding='utf-8') as f:
+        f.write(str(soup))
+
+    jsons = soup.find_all('script', type='application/ld+json')
+    for js in jsons:
+        if "Product" in json.loads(js.string).values():
+            state_json = json.loads(js.string)
+            item_name = state_json['name']
+            item_card = state_json['image']
+            item_statistic = state_json['offers']
+            item_price_with_card = item_statistic['price']
+            item_raiting = state_json['aggregateRating']['ratingValue']
+            item_number_of_comments = state_json['aggregateRating']['ratingCount']
+            item_price = None
+            item_article = None
+
+            noframes_tags = soup.find_all('noframes', attrs={'data-apiary': 'patch'})
+
+            # Перебираем найденные теги
+            for tag in noframes_tags:
+                content = tag.string.strip()
+                data = json.loads(content)
+                if "@card/VerifiedBadge" in data["widgets"].keys():
+                    item_article = find_gci(data)
+                if "@market/Ecommerce" in data["widgets"].keys() and item_price is None:
+                    item_price = find_gci(data, 'price')
+
+            d = {"item_name": item_name,
+                 "item_article": item_article,
+                 "link": link,
+                 "item_price": item_price,
+                 "item_price_with_card": item_price_with_card,
+                 "item_raiting": item_raiting,
+                 "item_number_of_comments": item_number_of_comments,
+                 "item_card": item_card}
+            return d
+
+
+def find_gci(data, v='gci'):
+    """Рекурсивно находит значение поля 'v'."""
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if key == v:
+                return value
+            else:
+                result = find_gci(value, v)
+                if result is not None:
+                    return result
+    elif isinstance(data, list):
+        for item in data:
+            result = find_gci(item, v)
+            if result is not None:
+                return result
+    return None
 
 
 def get_info_WB(product):
@@ -68,13 +142,67 @@ def get_info_WB(product):
     input_window.send_keys(Keys.ENTER)
     time.sleep(TIME_SLEEP)
 
-    item_links = driver.find_elements(By.CLASS_NAME, 'tile-clickable-element')
+    item_links = driver.find_elements(By.CLASS_NAME, 'product-card__link')
     item_links = list(set([f'{item.get_attribute("href")}' for item in item_links]))
     print(item_links)
+
+    for link in item_links:
+        info_of_product = find_info_item_wb(driver, link)
+        print(info_of_product)
+        time.sleep(1000)
+        return
 
     driver.close()
     driver.quit()
 
+
+def find_info_item_wb(driver, link):
+    driver.switch_to.new_window('tab')
+    driver.get(link)
+
+    soup = bs(str(driver.page_source), "lxml")
+    with open('one_pr_wb.html', 'w', encoding='utf-8') as f:
+        f.write(str(soup))
+
+    item_name = soup.find('h1', {'class': 'product-page__title'}).get_text(strip=True)
+    time.sleep(10000)
+    print(item_name)
+
+    # item_name = soup.find(attrs={"data-widget": "webProductHeading"}).get_text(strip=True)
+    #
+    # item_article = soup.find('div', string=lambda text: "Артикул:" in text).get_text(strip=True)
+    #
+    # find_data_state = soup.find_all('div', attrs={'data-state': True})
+    # item_price, item_price_with_card = None, None
+    #
+    # for element in find_data_state:
+    #     state_json = json.loads(element['data-state'])
+    #     if 'cardPrice' in state_json:
+    #         #print(state_json)
+    #         item_price = state_json['price']
+    #         item_price_with_card = state_json['cardPrice']
+    #         break
+    #
+    # state_json_2 = json.loads(soup.find('script', type='application/ld+json').string)
+    # item_card = state_json_2["image"]
+    # item_raiting = state_json_2["aggregateRating"]["ratingValue"]
+    # item_number_of_comments = state_json_2["aggregateRating"]["reviewCount"]
+    #
+    # d = {"item_name": item_name,
+    #      "item_article": item_article,
+    #      "link": link,
+    #      "item_price": item_price,
+    #      "item_price_with_card": item_price_with_card,
+    #      "item_raiting": item_raiting,
+    #      "item_number_of_comments": item_number_of_comments,
+    #      "item_card": item_card}
+
+    driver.close()  # закрываем данную вкладку
+    window_handles = driver.window_handles
+    first_tab_handle = window_handles[0]
+    driver.switch_to.window(first_tab_handle)  # возвращаемся на самую первую страницу
+
+    return 'z'
 
 
 def get_info_ozon(product):
@@ -119,7 +247,7 @@ def find_info_item(driver, link):
     for element in find_data_state:
         state_json = json.loads(element['data-state'])
         if 'cardPrice' in state_json:
-            #print(state_json)
+            # print(state_json)
             item_price = state_json['price']
             item_price_with_card = state_json['cardPrice']
             break
@@ -146,5 +274,6 @@ def find_info_item(driver, link):
     return d
 
 
-#get_info_ozon('тапочки')
-get_info_WB('тапочки')
+# get_info_ozon('тапочки')
+# get_info_WB('тапочки')
+get_info_Ymarket('самовар')
